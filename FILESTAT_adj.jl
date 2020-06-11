1d040d1f6a1bb1e957388756ad0e3ab0220c107b
Johannes Fleck <Jo-Fleck@users.noreply.github.com>
2020-06-11 16:44:24 +0200

    Algorithm with 98% same class

--- FILESTAT_adj.jl
+++ FILESTAT_adj.jl
@@ -8,13 +8,20 @@ using Plots, Plots.PlotMeasures, StatsPlots; gr()
 
 file_ASEC = "/Users/main/OneDrive - Istituto Universitario Europeo/data/ASEC/cps_00030.csv";
 dir_out = "/Users/main/Downloads/";
+file_out_temp = "tmp.csv"
+
 
 df_ASEC_0 = DataFrame(CSV.read(file_ASEC));
 
 filter!(r -> (r[:YEAR] .== 2003 || r[:YEAR] .== 2004), df_ASEC_0); # Keep only 2003 and 2004
 
 # Drop currently not needed vars
-select!(df_ASEC_0, Not([:MONTH, :CPSID, :CPSIDP,  :ASECFLAG, :ASECWTH, :ASECWT, :WKSWORK2, :WKSUNEM2, :FULLPART, :CAIDLY, :PMVCAID, :FFNGCAID, :SCHIPLY, :SCHLLUNCH, :FOODSTAMP]));
+select!(df_ASEC_0, Not([:MONTH, :CPSID, :ASECFLAG, :ASECWTH, :ASECWT, :EDUC, :STATEFIP, :OWNERSHP, :STAMPVAL, :FEDTAXAC, :DEPSTAT, :STATAXAC, :CPSIDP, :CTCCRD, :ACTCCRD, :EITCRED, :FEDRETIR, :CAIDLY, :PMVCAID, :FFNGCAID, :SCHIPLY]));
+
+#select!(df_ASEC_0,[:YEAR, :SERIAL, :HHINCOME, :PERNUM, :RELATE, :AGE, :SEX, :MARST, :FTOTVAL, :INCTOT, :ADJGINC, :FEDTAX, :FICA, :FILESTAT, :TAXINC]);
+
+df_ASEC_0[!, :FILESTAT_adj] = df_ASEC_0[!, :FILESTAT];
+
 
 # Plot FILESTAT histograms
 df_ASEC_2003 = filter(r -> r[:YEAR] == 2003, df_ASEC_0);
@@ -38,3 +45,91 @@ groupedbar(nam1, mn1, group = sx1, ylabel = "%", title = "FILESTAT Frequencies",
 
 # mn2 = FILESTAT_dist[1,:] - FILESTAT_dist[2,:]
 # bar(mn2)
+
+
+
+## Replicate FILESTAT for 2003
+
+df_ASEC_2003[!, :num] = 1:(size(df_ASEC_2003,1));
+
+hhs_2003 = unique(df_ASEC_2003.SERIAL);
+
+for k in hhs_2003
+
+    df_tmp = df_ASEC_2003[df_ASEC_2003.SERIAL .== k, :]
+    RELATE_vec = unique(df_tmp.RELATE)
+
+    if ~(201 in RELATE_vec)
+         continue # keep all FILESTAT as they are
+    else
+
+        num_vec = unique(df_tmp.num)
+
+        # hhs with taxable income > 0
+        age_101 = df_tmp[df_tmp.RELATE .== 101, :AGE][1]
+        age_201 = df_tmp[df_tmp.RELATE .== 201, :AGE][1]
+        if age_101 < 65 && age_201 < 65                     # Both below 65
+            df_ASEC_2003[ num_vec[1], :FILESTAT_adj] = 1
+            df_ASEC_2003[ num_vec[2], :FILESTAT_adj] = 1
+        elseif age_101 >= 65 && age_201 >= 65               # Both 65+
+            df_ASEC_2003[ num_vec[1], :FILESTAT_adj] = 3
+            df_ASEC_2003[ num_vec[2], :FILESTAT_adj] = 3
+        else                                                # One above, one below
+            df_ASEC_2003[ num_vec[1], :FILESTAT_adj] = 2
+            df_ASEC_2003[ num_vec[2], :FILESTAT_adj] = 2
+        end
+
+        # # hhs with taxable income = 0
+        # taxinc_101 = df_tmp[df_tmp.RELATE .== 101, :TAXINC][1]
+        # taxinc_201 = df_tmp[df_tmp.RELATE .== 201, :TAXINC][1]
+        # if taxinc_101 == 0 && taxinc_201 == 0
+        #     df_ASEC_2003[ num_vec[1], :FILESTAT_adj] = 6
+        #     df_ASEC_2003[ num_vec[2], :FILESTAT_adj] = 6
+        # end
+
+        # remaining hh members
+        if length(num_vec) > 2
+            for l = 3:length(num_vec)
+                df_ASEC_2003[ num_vec[l], :FILESTAT_adj] = df_ASEC_2003[ num_vec[l], :FILESTAT]
+            end
+        end
+    end
+end
+
+# Compute measures for mis classifications
+df_ASEC_2003[!, :delta_FILESTAT] = df_ASEC_2003[!, :FILESTAT] - df_ASEC_2003[!, :FILESTAT_adj];
+histogram(df_ASEC_2003.delta_FILESTAT)
+describe(df_ASEC_2003.delta_FILESTAT)
+N_same_class = count(i->(i == 0),df_ASEC_2003.delta_FILESTAT)
+pc_same_class = (N_same_class./size(df_ASEC_2003,1))*100
+
+
+
+df_ASEC_2003_mis = filter(r -> (r[:delta_FILESTAT] .!= 0), df_ASEC_2003);
+df_inspect = first(df_ASEC_2003_mis,100);
+CSV.write(dir_out * file_out_temp, df_inspect);
+
+
+# Fix resulting mis classifications
+
+df_ASEC_2003_mis[!, :num] = 1:(size(df_ASEC_2003_mis,1));
+
+hhs_2003_mis = unique(df_ASEC_2003_mis.SERIAL);
+
+for k in hhs_2003_mis
+
+    df_tmp = df_ASEC_2003_mis[df_ASEC_2003_mis.SERIAL .== k, :]
+
+    num_vec_mis = unique(df_tmp.num)
+    taxinc_101 = df_tmp[df_tmp.RELATE .== 101, :TAXINC][1]
+    taxinc_201 = df_tmp[df_tmp.RELATE .== 201, :TAXINC][1]
+
+    if taxinc_101 == 0 && taxinc_201 == 0
+        df_ASEC_2003_mis[ num_vec_mis[1], :FILESTAT_adj] = 6
+        df_ASEC_2003_mis[ num_vec_mis[2], :FILESTAT_adj] = 6
+    else
+
+    end
+
+
+end
